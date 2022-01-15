d56f98c02002d21f1edbe82bcf34dcb716fec173
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-01-15 11:16:06 +0100

    prototype1

--- prototype-1/prototype1.js
+++ prototype-1/prototype1.js
@@ -10,7 +10,7 @@ return reponse.json()
 .then(function(data){
 
 
-    outputHTML += "<table>"
+    outputHTML += "<table class = 'table table-dark table-striped'>"
     outputHTML += "<th>Produit</th>"
     outputHTML += "<th>Prix</th>"
 for (var i = 0; i < data.length; i++) {
@@ -22,9 +22,7 @@ for (var i = 0; i < data.length; i++) {
     outputHTML += "<td>" + data[i].prix +"</td>" ;
     outputHTML += "<tr>";
 
-    // outputHTML += "<tr>";
-    // outputHTML += "<td>" + data[i].produit +"</td>" ;
-    // outputHTML += "<tr>";
+   
 
     
 }
