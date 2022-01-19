5d2550fc487d824d149eb49b1469e8e238c2b2fa
hicham <ELmliki.hicham@gmail.com>
2022-01-19 04:14:14 -0800

    modif prototype 2

--- prototype-2/prototype2.js
+++ prototype-2/prototype2.js
@@ -13,22 +13,25 @@ return reponse.json()
 
 console.log(data)
     outputHTML += "<table class = 'table table-dark table-striped' >"
+    outputHTML += "<th>Id</th>"
     outputHTML += "<th>City</th>"
   outputHTML += "<th>Weather</th>"
   outputHTML += "<th>Wind-Speed </th>"  
-  outputHTML += "<th>Id</th>"
+  
     
 // for (var i = 0; i < data.length; i++) {
    
     outputHTML += "<tr>";
-   
+
+     outputHTML += "<td>" + data.id +"</td>" ;
+
     outputHTML += "<td>" + "  "+data.name +"</td>" ;
   
     outputHTML += "<td>" + data.weather[0].description  +"</td>" ;
 
     outputHTML += "<td>" + data.wind.speed +"</td>" ;
     
-    outputHTML += "<td>" + data.id +"</td>" ;
+  
 
 
     outputHTML += "<tr>";
