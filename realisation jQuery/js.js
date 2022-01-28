b09c2caef8096c218173851ee296349151cd4648
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-01-28 22:50:11 +0100

    realisation avec jQuery

--- realisation jQuery/js.js
+++ realisation jQuery/js.js
@@ -1,18 +1,31 @@
 
 
-var city
 
- var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bd0cd1e7d8ab7a578a5a4d28a57d45b&units=metric`
- 
- city = $("#input").val();
+
 
 $('#btn').click(function () { 
 
+$('#fromN').fadeIn();
+
+
+ var city = $("#input").val();
+
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bd0cd1e7d8ab7a578a5a4d28a57d45b&units=metric`
+    
+    
+
 $.get(url, 
-    function (data) {
-        console.log(data)
-        
+    function(data) {
+        $("#name").text(data.name),
+        $("#wind").text(data.wind.speed),
+        $("#weather").text(data.weather[0].description)
+        $('#IDcity').text("id:" +data.id)
+        $('#Temp').text(data.main.temp +" °C")   
+        // $('#iconM').src("http://openweathermap.org/img/w/"+data.weather[0].icon +".png");
+        document.getElementById('iconM').src = "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";
+
     },
-   
+    
+       
 );
 });
