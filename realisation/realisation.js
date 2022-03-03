0007dd85bc1dc1ea026d4b16af2c39c83b48e712
hicham <ELmliki.hicham@gmail.com>
2022-03-03 02:05:17 -0800

    modification langage

--- realisation/realisation.js
+++ realisation/realisation.js
@@ -12,7 +12,7 @@ function onSearch() {
     city = document.getElementById("input").value
     
 
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bd0cd1e7d8ab7a578a5a4d28a57d45b&units=metric`
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0bd0cd1e7d8ab7a578a5a4d28a57d45b&units=metric&lang=fr`
 
 fetch (url)
     .then(function(reponse) {
